dda7f7929ad986c22ea8a827493e21f37324fcc1
lhood1 <laurahood62@gmail.com>
2015-01-13 21:57:47 -0600

    Code 1-13-2015

---
+++ plybot/src/org/usfirst/frc/team5414/robot/Robot.java
@@ -0,0 +1,128 @@
+package org.usfirst.frc.team5414.robot;
+
+
+
+
+import edu.wpi.first.wpilibj.AnalogTrigger;
+import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj.buttons.JoystickButton;
+import edu.wpi.first.wpilibj.buttons.Trigger;
+import edu.wpi.first.wpilibj.livewindow.LiveWindow;
+
+/**
+ * The VM is configured to automatically run this class, and to call the
+ * functions corresponding to each mode, as described in the IterativeRobot
+ * documentation. If you change the name of this class or the package after
+ * creating this project, you must also update the manifest file in the resource
+ * directory.
+ */
+public class Robot extends IterativeRobot {
+	RobotDrive myRobot;
+	Joystick stick;
+	Joystick joystick1;
+	Joystick joystick2;
+	Trigger trigger1; 
+	
+	int autoLoopCounter;
+	JoystickButton aBtn;
+	JoystickButton bBtn;
+	JoystickButton xBtn;
+	JoystickButton yBtn;
+	JoystickButton LB;
+	JoystickButton RB;
+	Compressor c;
+	 DoubleSolenoid DoubleSolenoid1;
+    /**
+     * This function is run when the robot is first started up and should be
+     * used for any initialization code.
+     */
+    public void robotInit() {
+    	myRobot = new RobotDrive(0,1);
+    	stick = new Joystick(0);
+    	joystick1 = new Joystick(1);
+    	joystick2 = new Joystick(2);
+    	
+    	
+    	aBtn= new JoystickButton(stick, 1);
+    	bBtn = new JoystickButton(stick, 2);
+    	xBtn = new JoystickButton(stick, 3);
+    	yBtn = new JoystickButton(stick, 4);
+    	LB = new JoystickButton(stick, 5);
+    	RB = new JoystickButton(stick, 6);
+    	
+    	c = new Compressor(0);
+    	 DoubleSolenoid1 = new DoubleSolenoid(0,1);
+    }
+    
+    /**
+     * This function is run once each time the robot enters autonomous mode
+     */
+    public void autonomousInit() {
+    	autoLoopCounter = 0;
+    }
+
+    /**
+     * This function is called periodically during autonomous
+     */
+    public void autonomousPeriodic() {
+    	if(autoLoopCounter < 100) //Check if we've completed 100 loops (approximately 2 seconds)
+		{
+			myRobot.drive(-0.5, 0.0); 	// drive forwards half speed
+			autoLoopCounter++;
+			} else {
+			myRobot.drive(0.0, 0.0); 	// stop robot
+		}
+    }
+    
+    /**
+     * This function is called once each time the robot enters tele-operated mode
+     */
+    public void teleopInit(){
+    	c.setClosedLoopControl(true);
+    	DoubleSolenoid1 = new DoubleSolenoid(0,1);
+    }
+
+    /**
+     * This function is called periodically during operator control
+     */
+    public void teleopPeriodic() {
+        myRobot.tankDrive(stick.getRawAxis(1), stick.getRawAxis(5));
+        //myRobot.tankDrive(stick.getRawAxis(1), stick.getRawAxis(5));
+        //c.start();
+        //c.setClosedLoopControl(true);
+        
+//    	boolean enabled = c.enabled();
+//    	boolean pressureSwitch = c.getPressureSwitchValue();
+//    	float current = c.getCompressorCurrent();
+//    	DoubleSolenoid1.set(DoubleSolenoid.Value.kOff);
+    	if(LB.get())
+    	{
+    		System.out.println("a button true");
+    		DoubleSolenoid1.set(DoubleSolenoid.Value.kForward);
+    	}
+    	else if(RB.get())
+    	{
+    		DoubleSolenoid1.set(DoubleSolenoid.Value.kReverse);
+    		System.out.println("x button true");
+    	}
+    	else
+    	{
+    		DoubleSolenoid1.set(DoubleSolenoid.Value.kOff);
+//    		System.out.println("b button true");
+    	}
+    	
+    
+    
+    }
+
+	/**
+     * This function is called periodically during test mode
+     */
+    public void testPeriodic() {
+    	LiveWindow.run();
+    }
+    
+}
